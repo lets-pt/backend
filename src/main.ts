92c9cf2673072ef41745c1cdda456a741e7dd3d1
ChoSooBeen <cba0613060@gmail.com>
2023-07-17 02:28:46 +0900

    클라이언트에 대한 CORS 허용

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); //전역 파이프로 설정 : 모든 요청의 유효성 검사
+  app.enableCors({ //클라이언트에 대한 CORS 허용
+    origin: 'http://localhost:3000',
+  });
   await app.listen(3001);
 }
 bootstrap();
