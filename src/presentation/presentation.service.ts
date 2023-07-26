79ca82e371c595c770d181b5c20b8ed9631d26ed
ChoSooBeen <cba0613060@gmail.com>
2023-07-26 17:28:52 +0900

    presentation DB에 가공한 최종 영상 url 저장

--- src/presentation/presentation.service.ts
+++ src/presentation/presentation.service.ts
@@ -12,4 +12,26 @@ export class PresentationService {
     async createPresentation(createPresentationDTO: CreatePresentationDTO): Promise<Presentation> {
         return this.presentationModel.create(createPresentationDTO);
     }
+
+    // title로 Presentation 찾기
+    async findOneByTitle(title: string): Promise<Presentation> {
+        return this.presentationModel.findOne({ title: title });
+    }
+
+    //resultVideo - ffmpeg 결과물을 저장할 때 사용
+    async updateResultVideo(title: string, resultVideo: string): Promise<Presentation> {
+        try {
+            const presentation = await this.presentationModel.findOne({ title: title });
+
+            if (!presentation) {
+                throw new Error('Presentation not found');
+            }
+
+            presentation.resultVideo = resultVideo;
+            return presentation.save();
+        }
+        catch (err) {
+            throw new Error(err);
+        }
+    }
 }
