cd26e46ca2683562d3b4616f9a8c5f86e8f5e861
cod2048 <cod2048@gmail.com>
2023-07-31 00:43:12 +0900

    presentation dto 변경

--- src/presentation/dto/create-presentation.dto.ts
+++ src/presentation/dto/create-presentation.dto.ts
@@ -4,4 +4,9 @@ export class CreatePresentationDTO {
     pdfURL: string;
     recommendedWord: any[];
     forbiddenWord: any[];
+    sttScript: string;
+    comment: any[];
+    pdfTime: any[];
+    settingTime: any;
+    progressingTime: any;
 }
