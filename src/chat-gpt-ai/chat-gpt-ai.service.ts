f7ddd4f68b95699c9d0a08b4dfe9e62fd5596508
cod2048 <cod2048@gmail.com>
2023-08-08 02:19:42 +0900

    gpt qna부분 수정

--- src/chat-gpt-ai/chat-gpt-ai.service.ts
+++ src/chat-gpt-ai/chat-gpt-ai.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Configuration, OpenAIApi, CreateCompletionRequest} from 'openai';
+import { Configuration, OpenAIApi, CreateCompletionRequest } from 'openai';
 import { GetAiModelAnswer } from './model/get-ai-model-answer';
 import { PresentationService } from 'src/presentation/presentation.service';
 
@@ -9,57 +9,57 @@ const DEFAULT_MAX_TOKENS = 1000
 
 @Injectable()
 export class ChatGptAiService {
-    private readonly openAiApi:OpenAIApi
-    private selectedModelId:string|undefined
+    private readonly openAiApi: OpenAIApi
+    private selectedModelId: string | undefined
     private presentationService: PresentationService
-    constructor(){
+    constructor() {
         const configuration = new Configuration({
             organization: process.env.ORGANIZATION_ID,
             apiKey: process.env.OPENAI_API_KEY,
         });
         this.openAiApi = new OpenAIApi(configuration);
-    }   
+    }
 
-    setModelId(modelId:string){
+    setModelId(modelId: string) {
         this.selectedModelId = this.cleanModelId(modelId)
     }
 
-    cleanModelId(modelId:string){
-        if(modelId.includes(":")){
-            return modelId.replace(":","-")
+    cleanModelId(modelId: string) {
+        if (modelId.includes(":")) {
+            return modelId.replace(":", "-")
         }
         return modelId
     }
 
-    async listModels(){
+    async listModels() {
         const models = await this.openAiApi.listModels()
         return models.data
     }
 
-    async getModelAnswer(input:GetAiModelAnswer){
+    async getModelAnswer(input: GetAiModelAnswer) {
         try {
-            const {question,temperature,modelId,maxTokens} = input 
-             let model = DEFAULT_MODEL_ID
-             if(modelId){
+            const { question, temperature, modelId, maxTokens } = input
+            let model = DEFAULT_MODEL_ID
+            if (modelId) {
                 model = modelId
-             }else if(this.selectedModelId){
+            } else if (this.selectedModelId) {
                 model = this.selectedModelId
-             } 
-             
-             const prompt = `다음 스크립트를 간결하게 한국어로 다듬어주세요: ${question}`   
-             const params:CreateCompletionRequest ={
-                prompt: prompt,
-                model : this.cleanModelId(model),
-                temperature:temperature!=undefined?temperature:DEFAULT_TEMPERATURE,
-                max_tokens:maxTokens?maxTokens:DEFAULT_MAX_TOKENS
-             }
+            }
 
-             const response = await this.openAiApi.createCompletion(params)
-             const {data} =response
-            
-             let answer = data.choices[0]['text'].replace(/\n/g, '');
-             answer = answer.replace(/^\.+/, '');
-             return answer
+            const prompt = `다음 스크립트를 간결하게 한국어로 다듬어주세요: ${question}`
+            const params: CreateCompletionRequest = {
+                prompt: prompt,
+                model: this.cleanModelId(model),
+                temperature: temperature != undefined ? temperature : DEFAULT_TEMPERATURE,
+                max_tokens: maxTokens ? maxTokens : DEFAULT_MAX_TOKENS
+            }
+
+            const response = await this.openAiApi.createCompletion(params)
+            const { data } = response
+
+            let answer = data.choices[0]['text'].replace(/\n/g, '');
+            answer = answer.replace(/^\.+/, '');
+            return answer
 
         } catch (error) {
             console.log(error);
@@ -67,23 +67,23 @@ export class ChatGptAiService {
     }
 
 
-    async getModelQna(sttScript:string): Promise<string>{
+    async getModelQna(sttScript: string): Promise<string> {
         try {
-                       
-             const prompt = `다음 스크립트의 예상 질문과 간결한 답변 3가지 알려주세요: ${sttScript}`
 
-             const params:CreateCompletionRequest ={
+            const prompt = `다음 스크립트의 예상 질문과 간결한 답변 3가지 알려주세요: ${sttScript}`
+
+            const params: CreateCompletionRequest = {
                 prompt: prompt,
-                model : DEFAULT_MODEL_ID,
+                model: DEFAULT_MODEL_ID,
                 temperature: DEFAULT_TEMPERATURE,
                 max_tokens: DEFAULT_MAX_TOKENS
-             }
+            }
 
-             const response = await this.openAiApi.createCompletion(params)
-             const {data} =response
-            
-             let answer = data.choices[0]['text'].trim()
-                return answer = answer.replace(/Q/g, '\nQ').replace(/A/g, '\nA');
+            const response = await this.openAiApi.createCompletion(params)
+            const { data } = response
+
+            let answer = data.choices[0]['text'].trim()
+            return answer;
 
         } catch (error) {
             console.log(error);
--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
   app.enableCors({
-    origin: ['http://15.165.41.221:3000', 'http://15.165.41.221:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
   });
   await app.listen(3001);
